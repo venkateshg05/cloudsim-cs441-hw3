e9f7986f96c3c7dfac6d13af8d2471330464e37f
venkateshg05 <venkatesh_gopal@outlook.com>
2022-12-03 19:23:51 -0600

    (feat) add sim to test vm alloc policy

--- src/main/scala/HelperUtils/ObtainConfigReference.scala
+++ src/main/scala/HelperUtils/ObtainConfigReference.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 
 object ObtainConfigReference {
   private val config = ConfigFactory.load()
-  private val logger = CreateLogger(classOf[ObtainConfigReference])
+  private val logger = CreateLogger(classOf[ObtainConfigReference.type])
 
   private def ValidateConfig(confEntry: String): Boolean = Try(config.getConfig(confEntry)) match {
     case Failure(exception) => logger.error(s"Failed to retrieve config entry $confEntry for reason $exception"); false
